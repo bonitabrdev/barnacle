5e6ef16a304c680dee4203f9651c7b63a56f8c67
bonitabrdev <bonitabrdev@gmail.com>
2017-05-24 11:13:59 -0400

    Get rid of old routes

--- routes/web.php
+++ routes/web.php
@@ -22,17 +22,12 @@ Route::group(['prefix' => '/json', 'namespace' => 'Json', 'middleware' => 'auth'
     Route::put('/customers/{id}', 'CustomerController@update');
 
     Route::post('/reservations', 'ReservationController@store');
-
-    Route::post('/reservations/create/requests', 'CreateReservationRequestController@store');
-    Route::get('/reservations/create/requests/{id}/status', 'CreateReservationRequestController@status');
 });
 
 Route::group(['prefix' => '/reservations', 'middleware' => 'auth'], function () {
     Route::get('/create', 'ReservationController@create')
         ->name('reservation.create');
-    Route::get('/create/requests/{id}', 'ReservationController@showRequest');
     Route::get('/{id}', 'ReservationController@show');
-    Route::get('/create/failed', 'ReservationController@failed');
 });
 
 Route::group(['prefix' => '/admin'], function () {
@@ -47,47 +42,4 @@ Route::group(['prefix' => '/admin'], function () {
         ->name('admin.users.manage');
     Route::get('/settings', 'AdministrationController@manageSettings')
         ->name('admin.settings.manage');
-});
-
-// debug routes
-
-Route::group(['namespace' => 'Debug', 'prefix' => '/debug'], function () {
-    Route::get('/', function() {
-        return view('debug.home');
-    });
-
-    Route::get('/constraints/create', 'ConstraintController@create')
-        ->name('debug.constraint.create');
-    Route::post('/constraints', 'ConstraintController@store')
-        ->name('debug.constraint.store');
-    Route::get('/constraints/{id}', 'ConstraintController@show')
-        ->name('debug.constraint.show');
-    Route::get('/constraints', 'ConstraintController@index')
-        ->name('debug.constraint.index');
-    Route::get('/constraints/{id}/edit', 'ConstraintController@edit')
-        ->name('debug.constraint.edit');
-    Route::put('/constraints/{id}', 'ConstraintController@update')
-        ->name('debug.constraint.update');
-
-    Route::get('/customers/create', 'CustomerController@create')
-        ->name('debug.customer.create');
-    Route::post('/customers', 'CustomerController@store')
-        ->name('debug.customer.store');
-    Route::get('/customers/{id}', 'CustomerController@show')
-        ->name('debug.customer.show');
-    Route::get('/customers', 'CustomerController@index')
-        ->name('debug.customer.index');
-    Route::get('/customers/{id}/edit', 'CustomerController@edit')
-        ->name('debug.customer.edit');
-    Route::put('/customers/{id}', 'CustomerController@update')
-        ->name('debug.customer.update');
-
-    Route::get('/reservations', 'ReservationController@index')
-        ->name('debug.reservation.index');
-    Route::get('/reservations/create', 'ReservationController@create')
-        ->name('debug.reservation.create');
-    Route::get('/reservations/{id}', 'ReservationController@show')
-        ->name('debug.reservation.show');
-    Route::get('/reservations/{id}/edit', 'ReservationController@edit')
-        ->name('debug.reservations.edit');
 });
